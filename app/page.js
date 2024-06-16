03b60ac81455935aa29b6d7154637e34596c48aa
WebWithAstha <lodhiastha85@gmail.com>
2024-06-16 22:24:30 +0530

    static routing done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    <Navbar/>
     <h1 className=' mx-auto w-fit text-center text-4xl mt-60 border-b border-gray-400 py-2 text-gray-500'>Welcome to home page.</h1>
     </>
   )
---
+++ app/services/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 className=' mx-auto w-fit text-center text-4xl mt-60 border-b border-gray-400 py-2 text-gray-500'>Our services.</h1>
+
+  )
+}
+
+export default page
